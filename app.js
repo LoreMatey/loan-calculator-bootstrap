e864d2bb8c7f23c8263a120365b95135cf019f44
LoreMatey <lorena.matey.l@gmail.com>
2021-12-31 01:09:43 +0100

    Added alert message if numbers are wrong

--- app.js
+++ app.js
@@ -26,8 +26,36 @@ function calculateResults(e) {
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
   } else {
-    console.log('Please check numbers')
+    showError('Please check numbers');
   }
 
   e.preventDefault();
+}
+
+// Show error
+function showError(error) {
+  // Create a div
+  const errorDiv = document.createElement('div');
+
+  // Get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+
+  // Add class
+  errorDiv.className = 'alert alert-danger';
+
+  // Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 2000);     
+}
+
+// Clear error function
+function clearError() {
+  document.querySelector('.alert').remove();
 }
